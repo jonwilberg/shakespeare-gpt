a90a1d95aabcf1ef1cf050e6e4e4205999d00a22
Jon Riege <jonriege1996@gmail.com>
2023-11-23 16:05:24 +0800

    Add environment.yaml to replicate environment

--- config.py
+++ config.py
@@ -3,12 +3,13 @@
 # Preprocessing
 DATA_DIR = "data"
 TRAIN_PATH = DATA_DIR + "/spa.txt"
-NONBREAKING_SPA_PATH = DATA_DIR + "/nonbreaking_prefix.es"
-NONBREAKING_ENG_PATH = DATA_DIR + "/nonbreaking_prefix.en"
-EOS_TOKEN = "eos"
-SOS_TOKEN = "sos"
-VOCAB_SIZE = 10_000
+
 MAX_TOKENS = 64
+RESERVED_TOKENS = ["[PAD]", "[UNK]", "[START]", "[END]"]
+TOKENIZER_KWARGS = {"lower_case": True}
+
+# Tokenizer
+VOCAB_SIZE = 8000
 
 # Model training
 VAL_SHARE = 0.20
@@ -18,11 +19,7 @@ DROPOUT_RATE = 0.1
 CHECKPOINT_DIR = "checkpoints" 
 N_EPOCHS = 1
 PRINT_FREQ = 100
-OPTIMIZER_KWARGS = {
-    "beta_1": 0.9,
-    "beta_2": 0.98,
-    "epsilon": 1e-9
-}
+OPTIMIZER_KWARGS = {"beta_1": 0.9, "beta_2": 0.98, "epsilon": 1e-9}
 
 # Model structure
 D_MODEL = 64
